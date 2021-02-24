a10e26d8e35c02e760db9c75a42a0d94706cf596
Aayush Sanjar <sanjar14aayush@gmail.com>
2021-02-24 12:20:18 +0530

    coercion

--- Week1RBasics.R
+++ Week1RBasics.R
@@ -1,7 +1,28 @@
-x <- 1 #this is a numeric vector/object
+x <- 1 ## this is a numeric vector/object
 
-print(x)
-x # two ways of printing value of x
+print(x) ## explicit printing
+x ##auto-printing
 
-msg <- "Hello" #this is a sting vector
+msg <- "Hello" ## this is a sting vector
+
+x <- 1:20 ## : is used to create integer sequences
+x
+
+y <- vector("character", length = 10) ##vector()function
+y
+
+## implicit coercion
+z <- c(TRUE, 2,'abc') ##coercion to the Lowest Common Denominator
+z
+
+## explicit coercion
+x <- 0:5
+class(x)
+
+as.numeric(x)
+as.logical(x) 
+as.character(x)
+
+class(x) ## class remaining the same
+
 
